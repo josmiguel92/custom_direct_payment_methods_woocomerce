6fb293d1060bee7e17e76c4d3f9c0b9d93cd7cda
Josue M. Aguilar <josmiguel92@gmail.com>
2025-07-14 11:47:42 +0200

    Remove upgrade prompts from crypto and mobile money modals for a cleaner user experience.

--- settings/pages/crypto/modal2.php
+++ settings/pages/crypto/modal2.php
@@ -15,11 +15,5 @@
     <div class="digages-text-start digages-desktop-span-12 digages-tab-span-12 digages-mobile-span-12">
 
 
-<div class="modal-footer">
-<a href="https://digages.com/direct-payments-for-woocommerce/" target="_blank">
-      <button type="button" class="trddbtn22">Upgrade to PRO</button></a>
-<div class="tumazz_closse d-sm-none" data-bs-dismiss="modal" aria-label="Close">Close</div>
-</div>
-
 </div>
 </div>
